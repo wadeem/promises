3027502541a28052510edc12b0c1f08199c189e6
max3d <vadim@unikivi.ee>
2019-09-15 13:29:20 +0300

    clean up code with finally - spinner removal example

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 console.log('hello world!');
 
+//#region setup start
 const SW_API = 'https://swapi.co/api/';
 const output = document.getElementById('output');
+const spinner = document.getElementById('spinner');
 
-fetch(SW_API + 'films')
-    .then(
-        response => {
-            // if (!response.ok) throw new Error('error accessing the resource');
-            return response.json()
-                .then(films => {
-                    output.innerText = processResults(films.results);
-                });
-        },
-    ).catch(error => {
-    console.warn(error);
-    output.innerText = ':(';
-});
-
-function processResults(films) {
+const processTitles = (films) => {
     return films
         .sort((a, b) => a.episode_id - b.episode_id)
         .map(film => `${film.episode_id} ${film.title} ${film.release_date}`)
         .join('\n');
-}
+};
+//#region setup end
+
+fetch(SW_API + 'flms')
+    .then(response => {
+        if (!response.ok) throw new Error('bad response');
+        return response.json();
+    })
+    .then(json => {
+        const titles = processTitles(json.results);
+        output.innerText = titles;
+        return json.results;
+    })
+    .catch(error => {
+        output.innerText = error;
+        return [];
+    })
+    .finally(() => {
+        spinner.remove();
+    })
+    .then(films => console.log(films));
+
+
+
