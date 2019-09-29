7a1f8166172828c30ee7ee58647b4ae0e482d61a
max3d <vadim@unikivi.ee>
2019-09-29 10:35:17 +0300

    async await :)

---
+++ asyncawait.js
@@ -0,0 +1,22 @@
+const SW_API = 'https://swapi.co/api/';
+const out = document.getElementById('output');
+const spinner = document.getElementById('spinner');
+
+const fetchAPI = async (endpoint) => {
+    try {
+        const response = await fetch(SW_API + endpoint);
+        if (response.ok) return await response.json();
+        throw new Error(':(');
+    } catch (e) {
+        console.warn(e);
+    } finally {
+        spinner.remove();
+    }
+};
+
+const main = async () => {
+    const films = await fetchAPI('films');
+    out.innerText = `${films.count} films`;
+};
+
+main();
