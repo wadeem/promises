30b449a099bd988bd01d36f26310cac72f22f49a
max3d <vadim@unikivi.ee>
2019-09-29 08:28:50 +0300

    newer version of multiple parallel fetching with Promise.all([p1,p2,p3])

--- multiple.js
+++ multiple.js
@@ -1,18 +1,19 @@
+const SW_API = 'https://swapi.co/api/';
 const out = document.getElementById('output');
 const spin = document.getElementById('spinner');
-const SWAPI = 'https://swapi.co/api/';
-const fetchAPI = (endpoint) => {
-    return fetch(SWAPI + endpoint)
-        .then(res => {
-            return res.ok ? res.json() : Promise.reject(new Error('bad request'));
-        })
-};
+const fetchAPI = (endpoint) =>
+    fetch(SW_API + endpoint).then(r => r.ok ? r.json() : Promise.reject('err..'));
 
-const results = Promise.all([
+const promise = Promise.all([
     fetchAPI('films'),
-    fetchAPI('planets')
+    fetchAPI('planets'),
+    fetchAPI('species')
 ]);
 
-results.then(([f, p]) => {
-    out.innerText = `${f.count} films ${p.count} planets`;
+promise.then(arr => {
+    out.innerText =
+        `${arr[0].count} films, 
+         ${arr[1].count} planets 
+         ${arr[2].count} species`;
+    console.log(arr.length, 'requests successfully finished');
 }).finally(() => spin.remove());
